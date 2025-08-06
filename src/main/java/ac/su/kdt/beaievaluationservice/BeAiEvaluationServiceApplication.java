ac70a8d1ac58dc44f2e1618ef92f9d955c34105b
YeeunSeok <yes3641@naver.com>
2025-08-06 10:17:26 +0900

    Initial commit

---
+++ src/main/java/ac/su/kdt/beaievaluationservice/BeAiEvaluationServiceApplication.java
@@ -0,0 +1,13 @@
+package ac.su.kdt.beaievaluationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAiEvaluationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAiEvaluationServiceApplication.class, args);
+    }
+
+}
